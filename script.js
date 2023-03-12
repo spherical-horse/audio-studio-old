f51517a7305a09096aeec557b0e221c271948766
Artem <tema.shevchuk@gmail.com>
2023-03-12 21:57:33 +0300

    feat: add burger menu functionality

---
+++ script.js
@@ -0,0 +1,40 @@
+const burger = document.getElementById('burgerIcon');
+const burgerMenu = document.getElementById('topMenu');
+const burgerLines = document.querySelectorAll('.burger__line');
+const topMenuItems = document.querySelectorAll('.top-menu__li');
+
+const transformBurger = () => {
+  burgerLines.forEach(line => {
+    line.classList.toggle('open');
+  });
+}
+
+const closeBurgerMenu = () => {
+  burgerLines.forEach(line => {
+    line.classList.remove('open');
+  });
+  burgerMenu.classList.remove('open');
+  document.body.classList.remove('scroll-lock');
+};
+
+burger.addEventListener('click', () => {
+  burgerMenu.classList.toggle('open');
+  transformBurger();
+});
+
+const isInsideBurgerMenu = (target) => {
+  return Boolean(target.closest('.burger') || target.closest('.top-menu__nav'));
+};
+
+document.addEventListener('click', (e) => {
+  if (!isInsideBurgerMenu(e.target)) {
+    closeBurgerMenu();
+  }
+  e.stopImmediatePropagation();
+});
+
+topMenuItems.forEach(menuItem => {
+  menuItem.addEventListener('click', () => {
+    closeBurgerMenu();
+  });
+});
