a1831fb9a2f062538cda6f4eb968d4dcc38c7e0d
Artem <tema.shevchuk@gmail.com>
2023-08-11 20:42:26 +0300

    refactor: burger menu

--- script.js
+++ script.js
@@ -1,18 +1,9 @@
 const burger = document.getElementById('burgerIcon');
 const burgerMenu = document.getElementById('topMenu');
-const burgerLines = document.querySelectorAll('.burger__line');
 const topMenuItems = document.querySelectorAll('.top-menu__li');
 
-const transformBurger = () => {
-  burgerLines.forEach(line => {
-    line.classList.toggle('open');
-  });
-}
 
 const closeBurgerMenu = () => {
-  burgerLines.forEach(line => {
-    line.classList.remove('open');
-  });
   burgerMenu.classList.remove('open');
   document.body.classList.remove('scroll-lock');
 };
@@ -20,7 +11,6 @@ const closeBurgerMenu = () => {
 burger.addEventListener('click', () => {
   burgerMenu.classList.toggle('open');
   document.body.classList.toggle('scroll-lock');
-  transformBurger();
 });
 
 const isInsideBurgerMenu = (target) => {
